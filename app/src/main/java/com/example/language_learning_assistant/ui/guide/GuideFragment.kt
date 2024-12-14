55ac497c9c5549c908dda67999fc489aa3d3db69
DobosDavid74 <dobosdavid1974@gmail.com>
2024-12-14 11:39:23 +0100

    guide page added

--- app/src/main/java/com/example/language_learning_assistant/ui/guide/GuideFragment.kt
+++ app/src/main/java/com/example/language_learning_assistant/ui/guide/GuideFragment.kt
@@ -1,17 +1,16 @@
-package com.example.language_learning_assistant.ui.slideshow
+package com.example.language_learning_assistant.ui.guide
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.language_learning_assistant.databinding.FragmentSlideshowBinding
+import com.example.language_learning_assistant.databinding.FragmentGuideBinding
 
-class SlideshowFragment : Fragment() {
+class GuideFragment : Fragment() {
 
-    private var _binding: FragmentSlideshowBinding? = null
+    private var _binding: FragmentGuideBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,16 +21,17 @@ class SlideshowFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
+        val guideViewModel =
+            ViewModelProvider(this).get(GuideViewModel::class.java)
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        _binding = FragmentGuideBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textSlideshow
+
+        // Set the user guide text directly
+
+        //textView.text = """""".trimIndent()
         return root
     }
 
